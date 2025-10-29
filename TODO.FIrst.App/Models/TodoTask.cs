c86d873bf4a3b009849688ca1757a86427b3d261
Fredrik von Post <fredrik.v.post@gmail.com>
2025-10-29 13:46:29 +0100

    Add DataModel

---
+++ TODO.FIrst.App/Models/TodoTask.cs
@@ -0,0 +1,11 @@
+namespace TODO.FIrst.App.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
